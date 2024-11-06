96f4325862f7002c54a7c400e5b93a20ad6c4d69
Bendix-Schmidt <bendix.schmidt2000@gmail.com>
2024-11-06 19:38:16 +0100

    ex02 done

--- CPP_02/ex02/src/Fixed.cpp
+++ CPP_02/ex02/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: bschmidt <bschmidt@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 19:22:49 by bschmidt          #+#    #+#             */
-/*   Updated: 2024/11/06 17:52:27 by bschmidt         ###   ########.fr       */
+/*   Updated: 2024/11/06 19:37:59 by bschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,18 +50,88 @@ Fixed	&Fixed::operator=(const Fixed& original)
 	return (*this);
 }
 
-//Arithmetic Operators:
-		Fixed operator+(const Fixed &rhs) const;
-   		Fixed operator-(const Fixed &rhs) const;
-		Fixed operator*(const Fixed &rhs) const;
-   		Fixed operator/(const Fixed &rhs) const;
+//Arithmetic Operators (the "this->" is optional, but easier to understand):
+Fixed Fixed::operator+(const Fixed &second) const
+{
+	return (Fixed(this->toFloat()) + second.toFloat());
+}
 
+Fixed Fixed::operator-(const Fixed &second) const
+{
+	return (Fixed(this->toFloat()) - second.toFloat());
+}
 
+Fixed Fixed::operator*(const Fixed &second) const
+{
+	return (Fixed(this->toFloat()) * second.toFloat()); 
+}
 
+Fixed Fixed::operator/(const Fixed &second) const
+{
+	return (Fixed(this->toFloat()) / second.toFloat()); 
+}
 
+//Increment and Decrement operators (int in brackets means its post-increment/decrement):
+Fixed Fixed::operator++(int)
+{
+	Fixed tmp = *this;
 
+	++(this->fp_nb_val);
+	return (tmp);
+}
+		
+Fixed Fixed::operator--(int)
+{
+	Fixed tmp = *this;
 
+	--(this->fp_nb_val);
+	return (tmp);
+}
 
+Fixed Fixed::operator++(void)
+{
+	this->fp_nb_val += 1;
+	return (*this);
+}
+
+Fixed Fixed::operator--(void)
+{
+	this->fp_nb_val -= 1;
+	return (*this);
+}
+
+//MIN and MAX functions
+Fixed& Fixed::min(Fixed &first, Fixed &second)
+{
+    if (first.getRawBits() > second.getRawBits())
+        return (second);
+	else
+        return (first);
+}
+
+Fixed& Fixed::min(const Fixed &first, const Fixed &second)
+{
+    if (first.getRawBits() > second.getRawBits())
+        return ((Fixed&)second);
+	else
+        return ((Fixed&)first);
+}
+
+Fixed& Fixed::max(Fixed &first, Fixed &second)
+{
+    if (first.getRawBits() > second.getRawBits())
+        return (first);
+	else
+        return (second);
+}
+
+Fixed& Fixed::max(const Fixed &first, const Fixed &second)
+{
+    if (first.getRawBits() > second.getRawBits())
+        return ((Fixed&)first);
+	else
+        return ((Fixed&)second);
+}
 
 
 
