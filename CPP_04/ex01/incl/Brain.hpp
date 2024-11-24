33e0296783cb84f0ad2a5d4524f702db8a3ced00
Bendix-Schmidt <bendix.schmidt2000@gmail.com>
2024-11-24 17:26:47 +0100

    ex01 fancy shit

--- CPP_04/ex01/incl/Brain.hpp
+++ CPP_04/ex01/incl/Brain.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Brain.Hpp                                          :+:      :+:    :+:   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bschmidt <bschmidt@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 17:52:45 by bschmidt          #+#    #+#             */
-/*   Updated: 2024/11/22 17:56:14 by bschmidt         ###   ########.fr       */
+/*   Updated: 2024/11/24 17:20:15 by bschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,20 @@ class Brain
 {
 	private:
 		std::string ideas[100];			
+
+	public:
+		//getter
+		std::string	get_idea(int i) const;
+		
+		//setter
+		void		set_idea(int i, std::string idea);
+		
+		//printers
+		void 		print_first_idea(std::ostream &stream) const;
+		void		print_specific_idea(std::ostream &stream, int index) const;
+		void		print_all_ideas(std::ostream &stream) const;
 };
+
+std::ostream	&operator<<(std::ostream &stream, Brain const &brain);
 
 #endif // BRAIN_HPP
--- CPP_04/ex01/src/Brain.cpp
+++ CPP_04/ex01/src/Brain.cpp
@@ -6,30 +6,95 @@
 /*   By: bschmidt <bschmidt@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 17:56:22 by bschmidt          #+#    #+#             */
-/*   Updated: 2024/11/22 18:01:39 by bschmidt         ###   ########.fr       */
+/*   Updated: 2024/11/24 17:26:26 by bschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
+//member functions:
+//getter
+std::string	Brain::get_idea(int i) const
+{
+	return (this->ideas[i]);
+}
 
+//setter
+void	Brain::set_idea(int i, std::string idea)
+{
+	this->ideas[i] = idea;
+}
 
+//printers
+void	Brain::print_first_idea(std::ostream &stream) const
+{
+	if (ideas[0].empty())
+		stream << "Idea[0] does not exist" << std::endl;
+	else
+		stream << ideas[0] << std::endl;
+}
 
+void	Brain::print_specific_idea(std::ostream &stream, int index) const
+{
+	if (index < 0 || index >= 100)
+	{
+		stream << "Index out of Idea-range." << std::endl;
+		return ;
+	}
+	if (ideas[index].empty())
+		stream << "Idea[" << index << "] does not exist." << std::endl;
+	else
+		stream << ideas[index] << std::endl;	
+}
 
+void	Brain::print_all_ideas(std::ostream &stream) const
+{
+	int i = 0;
+	
+	while (i < 100)
+	{
+		if (!ideas[i].empty())
+			print_specific_idea(stream, i);
+		i++;
+	}
+}
 
-//constructor
+/*-----------------------------------------------------------------------*/
+//standard constructor
 Brain::Brain()
-{};
+{
+	std::cout << "Brain default constructor called" << std::endl;
+};
 
 //copy constructor
 Brain::Brain(const Brain &original)
 {
+	std::cout << "Brain copied." << std::endl;
 	*this = original;
 }
 
 //copy assigenment operator
 Brain	&Brain::operator=(const Brain &original)
 {
-	
+    if (this != &original)
+	{
+        for (int i = 0; i < 100; ++i)
+		{
+            this->ideas[i] = original.ideas[i];
+    	}
+    return *this;
+	}
 }
 
+//standard destructor
+Brain::~Brain()
+{
+	std::cout << "Brain destructor called" << std::endl;
+}
+
+//overloads
+std::ostream	&operator<<(std::ostream &stream, Brain const &brain)
+{
+	stream << brain.get_idea(0) << std::endl;
+	return (stream);
+}
