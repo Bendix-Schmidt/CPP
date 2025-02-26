055d19c4568be6fa1d8cfa417dbb9265c62fbbad
Bendix-Schmidt <bendix.schmidt2000@gmail.com>
2025-02-26 15:40:24 +0100

    started CPP07 and fixed makefile in 07ex02

---
+++ CPP_07/ex00/incl/whatever.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bschmidt <bschmidt@student.42.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 15:29:48 by bschmidt          #+#    #+#             */
+/*   Updated: 2025/02/26 15:37:10 by bschmidt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T a, T b)
+{
+	T tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	return (a > b ? a : b);
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	return (a > b ? b : a);
+}
+
+#endif // WHATEVER_HPP
