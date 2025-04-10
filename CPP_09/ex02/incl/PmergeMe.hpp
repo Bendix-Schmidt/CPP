6907fe742ebd63edef345d6cdec287b820bb0d28
Bendix-Schmidt <bendix.schmidt2000@gmail.com>
2025-04-10 19:17:46 +0200

    finished ex02

--- CPP_09/ex02/incl/PmergeMe.hpp
+++ CPP_09/ex02/incl/PmergeMe.hpp
@@ -5,8 +5,54 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bschmidt <bschmidt@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/09 16:42:02 by bschmidt          #+#    #+#             */
-/*   Updated: 2025/04/09 16:42:03 by bschmidt         ###   ########.fr       */
+/*   Created: 2025/04/10 19:13:10 by bschmidt          #+#    #+#             */
+/*   Updated: 2025/04/10 19:13:12 by bschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+#include <vector>
+#include <deque>
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
+#include <climits>
+
+class PmergeMe
+{
+	private:
+		std::vector<int> vec;
+		std::deque<int> deque;
+		double _vectorTime;
+		double _dequeTime;
+	
+	public:
+		void sort();
+		void displayBefore() const;
+		void displayAfter() const;
+
+		//constructors && destructor
+		PmergeMe();
+		PmergeMe(int argc, char **argv);
+		PmergeMe(const PmergeMe& other);
+		PmergeMe& operator=(const PmergeMe& other);
+		~PmergeMe();
+		
+	private:
+		void sortVector();
+		void sortDeque();
+		void mergeInsertVector(std::vector<int>& vec);
+		void mergeInsertDeque(std::deque<int>& dq);
+		bool isPositiveNumber(const std::string& str) const;
+		int jacobsthalNumber(int n) const;
+		std::vector<int> generateJacobsthalInsertionOrder(int n) const;
+};
+
+#endif // PmergeMe_HPP
--- CPP_09/ex02/src/PmergeMe.cpp
+++ CPP_09/ex02/src/PmergeMe.cpp
@@ -5,8 +5,327 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bschmidt <bschmidt@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/09 13:34:24 by bschmidt          #+#    #+#             */
-/*   Updated: 2025/04/09 16:41:58 by bschmidt         ###   ########.fr       */
+/*   Created: 2025/04/10 19:12:57 by bschmidt          #+#    #+#             */
+/*   Updated: 2025/04/10 19:12:59 by bschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../incl/PmergeMe.hpp"
+
+
+PmergeMe::PmergeMe() {}
+
+PmergeMe::PmergeMe(int argc, char **argv)
+{
+    vec.reserve(argc - 1); //preallocate memory to stop my vector from being moved around a lot :)
+    for (int i = 1; i < argc; i++)
+    {
+        vec.push_back(atoi(argv[i]));
+		deque.push_back(atoi(argv[i]));
+    }
+}
+
+PmergeMe::~PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe& oth) : vec(oth.vec), deque(oth.deque) {}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& oth)
+{
+	if (this != &oth)
+	{
+		vec = oth.vec;
+		deque = oth.deque;
+	}
+	return *this;
+}
+
+void PmergeMe::displayBefore() const
+{
+	std::cout << "Before: ";
+	for (std::vector<int>::const_iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::displayAfter() const
+{
+	std::cout << "After: ";
+	for (std::vector<int>::const_iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+}
+
+// Main sorting functions
+void PmergeMe::sortVector()
+{
+	if (vec.size() <= 1)
+		return;
+	mergeInsertVector(vec);
+}
+
+void PmergeMe::sortDeque()
+{
+	mergeInsertDeque(deque);
+}
+
+// Helper function to calculate Jacobsthal numbers
+int PmergeMe::jacobsthalNumber(int n) const
+{
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	
+	int j_n_minus_1 = 1;
+	int j_n_minus_2 = 0;
+	int j_n = 0;
+	
+	for (int i = 2; i <= n; i++)
+	{
+		j_n = j_n_minus_1 + 2 * j_n_minus_2;
+		j_n_minus_2 = j_n_minus_1;
+		j_n_minus_1 = j_n;
+	}
+	
+	return j_n;
+}
+
+// Generates the Jacobsthal insertion order for n elements
+std::vector<int> PmergeMe::generateJacobsthalInsertionOrder(int n) const
+{
+	std::vector<int> result;
+	if (n <= 0) return result;
+	
+	// Berechne alle benötigten Jacobsthal-Zahlen
+	std::vector<int> jacobsthalNumbers;
+	jacobsthalNumbers.push_back(0);  // J(0) = 0
+	jacobsthalNumbers.push_back(1);  // J(1) = 1
+	
+	int i = 2;
+	while (jacobsthalNumbers.back() < n) {
+		int next = jacobsthalNumbers[i-1] + 2 * jacobsthalNumbers[i-2];
+		jacobsthalNumbers.push_back(next);
+		i++;
+	}
+	
+	// Erstelle die Einfügereihenfolge
+	std::vector<bool> used(n + 1, false);
+	
+	// Füge zuerst 1 ein (immer der erste)
+	result.push_back(1);
+	used[1] = true;
+	
+	// Füge dann die restlichen Elemente gemäß der Jacobsthal-Reihenfolge ein
+	for (size_t j = 3; j < jacobsthalNumbers.size(); j++) {
+		// Füge die Jacobsthal-Position ein
+		int pos = jacobsthalNumbers[j];
+		if (pos <= n && !used[pos]) {
+			result.push_back(pos);
+			used[pos] = true;
+		}
+		
+		// Füge alle Positionen zwischen der aktuellen und vorherigen Jacobsthal-Zahl ein
+		for (int k = pos - 1; k > jacobsthalNumbers[j-1]; k--) {
+			if (k <= n && !used[k]) {
+				result.push_back(k);
+				used[k] = true;
+			}
+		}
+	}
+	
+	// Füge alle verbleibenden Positionen ein
+	for (int j = 1; j <= n; j++) {
+		if (!used[j]) {
+			result.push_back(j);
+		}
+	}
+	return result;
+}
+
+void PmergeMe::mergeInsertVector(std::vector<int>& vec)
+{
+	if (vec.size() <= 1)
+		return;
+
+	// build pairs
+	std::vector<std::pair<int, int> > pairs;
+	int single = -1;
+	size_t i = 0;
+	
+	while (i < vec.size())
+	{
+		int first = vec[i++];
+		if (i < vec.size())
+		{
+			int second = vec[i++];
+			if (first > second)
+				pairs.push_back(std::make_pair(second, first));
+			else
+				pairs.push_back(std::make_pair(first, second));
+		}
+		else
+			single = first;
+	}
+
+	// sort vector with bigger elements
+	std::vector<int> sorted;
+	for (std::vector<std::pair<int, int> >::iterator it = pairs.begin(); 
+		 it != pairs.end(); ++it)
+	{
+		sorted.push_back(it->second);
+	}
+
+	if (sorted.size() > 1)
+		mergeInsertVector(sorted);
+
+	// Store the smaller elements in a vector for easier access
+	std::vector<int> smallerElements;
+	for (std::vector<std::pair<int, int> >::iterator it = pairs.begin(); 
+		 it != pairs.end(); ++it)
+	{
+		smallerElements.push_back(it->first);
+	}
+	
+	// Generate the Jacobsthal insertion order
+	std::vector<int> insertionOrder = generateJacobsthalInsertionOrder(smallerElements.size());
+
+	// Insert the smaller elements according to the Jacobsthal sequence
+	for (size_t i = 0; i < insertionOrder.size(); i++)
+	{
+		int index = insertionOrder[i] - 1; // -1 because indices start at 0
+		if (index < static_cast<int>(smallerElements.size()))
+		{
+			int valueToInsert = smallerElements[index];
+			
+			// For vector we can use binary search
+			std::vector<int>::iterator pos = std::lower_bound(sorted.begin(), sorted.end(), valueToInsert);
+			sorted.insert(pos, valueToInsert);
+		}
+	}
+
+	// push single element
+	if (single != -1)
+	{
+		// Binary search for the single element
+		std::vector<int>::iterator pos = std::lower_bound(sorted.begin(), sorted.end(), single);
+		sorted.insert(pos, single);
+	}
+	vec = sorted;
+}
+
+void PmergeMe::mergeInsertDeque(std::deque<int>& dq)
+{
+	if (dq.size() <= 1)
+		return;
+
+	// build pairs
+	std::deque<std::pair<int, int> > pairs;
+	int single = -1;
+	std::deque<int>::iterator it = dq.begin();
+	
+	while (it != dq.end())
+	{
+		int first = *it++;
+		if (it != dq.end())
+		{
+			int second = *it++;
+			if (first > second)
+				pairs.push_back(std::make_pair(second, first));
+			else
+				pairs.push_back(std::make_pair(first, second));
+		}
+		else
+			single = first;
+	}
+
+	// sort deque with bigger elements
+	std::deque<int> sorted;
+	for (std::deque<std::pair<int, int> >::iterator it = pairs.begin(); 
+		 it != pairs.end(); ++it)
+	{
+		sorted.push_back(it->second);
+	}
+
+	if (sorted.size() > 1)
+		mergeInsertDeque(sorted);
+
+	// Store the smaller elements in a vector for easier access
+	std::vector<int> smallerElements;
+	for (std::deque<std::pair<int, int> >::iterator it = pairs.begin(); 
+		 it != pairs.end(); ++it)
+	{
+		smallerElements.push_back(it->first);
+	}
+	
+	// Generate the Jacobsthal insertion order
+	std::vector<int> insertionOrder = generateJacobsthalInsertionOrder(smallerElements.size());
+	
+	// Insert the smaller elements according to the Jacobsthal sequence
+	for (size_t i = 0; i < insertionOrder.size(); i++)
+	{
+		int index = insertionOrder[i] - 1; // -1 because indices start at 0
+		if (index < static_cast<int>(smallerElements.size()))
+		{
+			int valueToInsert = smallerElements[index];
+			
+			// For deque we can use binary search
+			size_t left = 0;
+			size_t right = sorted.size();
+			
+			while (left < right)
+			{
+				size_t mid = left + (right - left) / 2;
+				if (sorted[mid] < valueToInsert)
+					left = mid + 1;
+				else
+					right = mid;
+			}
+			
+			sorted.insert(sorted.begin() + left, valueToInsert);
+		}
+	}
+
+	// push single element
+	if (single != -1)
+	{
+		// Binary search for the single element
+		size_t left = 0;
+		size_t right = sorted.size();
+		
+		while (left < right)
+		{
+			size_t mid = left + (right - left) / 2;
+			if (sorted[mid] < single)
+				left = mid + 1;
+			else
+				right = mid;
+		}
+		
+		sorted.insert(sorted.begin() + left, single);
+	}
+	
+	dq = sorted;
+}
+
+void PmergeMe::sort()
+{
+	clock_t startVector = clock();
+	sortVector();
+	clock_t endVector = clock();
+	_vectorTime = static_cast<double>(endVector - startVector) / CLOCKS_PER_SEC * 1000000;
+
+	clock_t startDeque = clock();
+	sortDeque();
+	clock_t endDeque = clock();
+	_dequeTime = static_cast<double>(endDeque - startDeque) / CLOCKS_PER_SEC * 1000000;
+
+	std::cout << "Time to process a range of " << vec.size() 
+			  << " elements with std::vector : " << std::fixed 
+			  << std::setprecision(5) << _vectorTime << " us" << std::endl;
+	
+	std::cout << "Time to process a range of " << deque.size() 
+			  << " elements with std::deque : " << std::fixed 
+			  << std::setprecision(5) << _dequeTime << " us" << std::endl;
+} 
